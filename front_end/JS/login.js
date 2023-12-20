669e41223b50725cab44e8d019826f8ea095f16f
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-12-20 22:06:49 +0100

    Update login.js

--- front_end/JS/login.js
+++ front_end/JS/login.js
@@ -6,7 +6,7 @@ function sigin_login() {
   /*Check username and password*/
   if (username == "admin" && password == "admin") {
     alert("enter");
-    window.location.href = "signup.html";
+    window.location.href = "https://portfolio-neuro-nutrire.vercel.app/signup.html";
   } else {
     alert("Wrong Username or Password! Please try again.");
   }
