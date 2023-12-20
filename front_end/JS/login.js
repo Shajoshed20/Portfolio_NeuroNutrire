2206fb4415d4d6be2f62e55fe6d6afe5b95d599d
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-12-20 05:48:04 +0100

    Update login.js

--- front_end/JS/login.js
+++ front_end/JS/login.js
@@ -1,5 +1,5 @@
 //Login Button EventListener
-document.getElementById("LoginBtn").addEventListener("click", function(){
+function sigin_login() {
     var username = document.getElementById('user').value;
     var password = document.getElementById('password').value;
 
