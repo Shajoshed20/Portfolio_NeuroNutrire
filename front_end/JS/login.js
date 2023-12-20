ebf8baff8f5b90b53f3bb583c83c1106c591f130
Iyioluwa Awe <86264371+iyiolakeni@users.noreply.github.com>
2023-12-20 21:45:33 +0100

    Update login.js

--- front_end/JS/login.js
+++ front_end/JS/login.js
@@ -6,6 +6,7 @@ function sigin_login() {
   /*Check username and password*/
   if (username == "admin" && password == "admin") {
     alert("enter");
+    window.location.href = "dasboard.html";
   } else {
     alert("Wrong Username or Password! Please try again.");
   }
